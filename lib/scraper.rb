e60e2c4ad11b998b7b0d1c1e9a1fdafecdd3c8e6
Olivia Cubela <oliviacubela@gmail.com>
2020-07-20 00:11:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,12 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    
     binding.pry
   end
+  
 end
+
 Scraper.new.get_page
 
   def print_courses

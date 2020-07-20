56f9de167e2ff565f2815d1bf50d2fed20d03484
Olivia Cubela <oliviacubela@gmail.com>
2020-07-20 00:59:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
-require 'pry'
+# require 'pry'
 
 require_relative './course.rb'
 
@@ -22,18 +22,17 @@ class Scraper
       course.description = post.css("p").text
     end
   end
-end
 
-Scraper.new.print_courses
-
-def print_courses
-  self.make_courses
-  Course.all.each do |course|
-    if course.title && course.title != ""
-      puts "Title: #{course.title}"
-      puts "  Schedule: #{course.schedule}"
-      puts "  Description: #{course.description}"
-      binding.pry
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+        # binding.pry
+      end
     end
   end
 end
+Scraper.new.print_courses
